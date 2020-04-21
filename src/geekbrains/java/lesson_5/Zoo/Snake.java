612018269fb411a2f5639c4dd49d01b9b6ecb60f
Shaligin <redgy1@yandex.ru>
2020-04-21 10:46:31 +0300

    homework_5

---
+++ src/geekbrains/java/lesson_5/Zoo/Snake.java
@@ -0,0 +1,22 @@
+//package geekbrains.java.lesson_5.Zoo;
+//
+//
+//import geekbrains.java.lesson_5.Animal;
+//
+//public class Snake extends Animal {
+//
+//    public Snake(String name, String color, int age) {
+//        super(name, color, age);
+//    }
+//
+//    @Override
+//    public void voice() {
+//        System.out.println(name + " SSShhhhh");
+//    }
+//
+//
+//    @Override
+//    protected void move() {
+//        System.out.println(name + " crawls");
+//    }
+//}
